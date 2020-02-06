3d9869835882e7280d1dd6c9dc7449de2b651e35
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-06 15:33:02 -0500

    added event listeners and numbers array

--- js/script.js
+++ js/script.js
@@ -6,4 +6,26 @@ const input = document.querySelector('#input'), // input/output button
   result = document.querySelector('#result'), // equal button
   clear = document.querySelector('#clear'); // clear button
 
+const numbersArray = [];
+const operatorsArray = [];
+const inputArray = [];
+
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+// place numbers on screen and in array on click
+numbers.forEach(function(number) {
+  number.addEventListener('click', function(event) {
+    event.preventDefault();
+    numbersArray.push(number);
+    input.innerHTML += number.innerHTML;
+  });
+});
+
+// place operators in screen and in array on click
+operators.forEach(function(operator) {
+  operator.addEventListener('click', function(event) {
+    event.preventDefault();
+    operatorsArray.push(operator);
+    input.innerHTML += operator.innerHTML;
+  });
+});
